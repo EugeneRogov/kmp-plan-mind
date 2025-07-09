0089498903dd463c7e2924f13a1ecb703d7a192a
Eugene Rogov <eugeniirogov@MacBook-Pro-Eugenii.local>
2025-07-09 18:41:18 +0300

    Koin refactor: Extract Koin initialization into a common module
    This commit refactors the Koin dependency injection setup by:
    
    - Moving the Koin initialization logic into a new `KoinInit.kt` file within the `commonMain` source set.
    - Calling the `initKoin()` function from the common `PlanMindApp` composable.
    - Removing the Android-specific Koin initialization from the `App` class in `androidMain`.
    
    This change centralizes Koin initialization, making it easier to manage and ensuring consistency across platforms.

--- composeApp/src/androidMain/kotlin/com/eugenerogov/planmind/App.kt
+++ composeApp/src/androidMain/kotlin/com/eugenerogov/planmind/App.kt
@@ -1,29 +1,5 @@
 package com.eugenerogov.planmind
 
 import android.app.Application
-import com.eugenerogov.planmind.di.serviceApiModule
-import com.eugenerogov.planmind.di.repositoryModule
-import com.eugenerogov.planmind.di.useCasesModule
-import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
-import org.koin.core.context.startKoin
 
-class App : Application() {
-    override fun onCreate() {
-        super.onCreate()
-
-        // region Koin
-        startKoin {
-            androidLogger()
-            androidContext(this@App)
-            modules(
-                listOf(
-                    useCasesModule,
-                    repositoryModule,
-                    serviceApiModule,
-                )
-            )
-        }
-        // endregion
-    }
-}
+class App : Application()
