9364ee085a1146d997f338077dbeb34b6bebeff3
Eugene Rogov <eugeniirogov@MacBook-Pro-Eugenii.local>
2025-07-09 22:19:05 +0300

    Implement `ProfileService` and `ProfileRepository` to fetch user profile data
    This commit introduces the implementation for `ProfileService` and `ProfileRepositoryImpl`.
    
    - `ProfileServiceImpl` now handles the network request to fetch user profile data using Ktor client. It returns an `Either` type to represent success or failure.
    - `ProfileRepositoryImpl` utilizes `ProfileService` to get the user profile and simply passes the result through. The error mapping logic is commented out for now.

--- shared/src/commonMain/kotlin/com/eugenerogov/planmind/data/remote/ProfileService.kt
+++ shared/src/commonMain/kotlin/com/eugenerogov/planmind/data/remote/ProfileService.kt
@@ -1,11 +1,28 @@
 package com.eugenerogov.planmind.data.remote
 
+import arrow.core.Either
+import com.eugenerogov.planmind.Failure
+import com.eugenerogov.planmind.domain.entities.profile.UserProfile
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import com.eugenerogov.planmind.data.remote.core.Endpoint.USER_PROFILE
 
 interface ProfileService {
+
+    suspend fun getUserProfile(): Either<Failure, UserProfile>
 
 }
 
 class ProfileServiceImpl(private val client: HttpClient) : ProfileService {
+
+    override suspend fun getUserProfile(): Either<Failure, UserProfile> {
+        return try {
+            val profile: UserProfile = client.get(USER_PROFILE).body()
+            Either.Right(profile)
+        } catch (e: Exception) {
+            Either.Left(Failure.NetworkError(e.message ?: "Unknown error"))
+        }
+    }
 
 }
