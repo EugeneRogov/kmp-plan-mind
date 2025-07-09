aeb8cd942741c14bb08c30c118ad40f5570d862d
Eugene Rogov <eugeniirogov@MacBook-Pro-Eugenii.local>
2025-07-09 22:07:36 +0300

    Added DI for ProfileService in ProfileRepositoryImpl

--- shared/src/commonMain/kotlin/com/eugenerogov/planmind/data/ProfileRepositoryImpl.kt
+++ shared/src/commonMain/kotlin/com/eugenerogov/planmind/data/ProfileRepositoryImpl.kt
@@ -1,16 +1,16 @@
 package com.eugenerogov.planmind.data
 
 import arrow.core.Either
-import arrow.core.flatMap
 import com.eugenerogov.planmind.Failure
-import com.eugenerogov.planmind.domain.AuthRepository
+import com.eugenerogov.planmind.data.remote.ProfileService
 import com.eugenerogov.planmind.domain.ProfileRepository
-import com.eugenerogov.planmind.domain.entities.SignIn
 import com.eugenerogov.planmind.domain.entities.profile.UserProfile
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
 class ProfileRepositoryImpl(
-
-) : ProfileRepository {
+) : ProfileRepository, KoinComponent {
+    private val profileService: ProfileService by inject()
     override suspend fun getUserProfile(): Either<Failure, UserProfile> {
         TODO("Not yet implemented")
     }
