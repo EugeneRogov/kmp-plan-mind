f0829b0382a2e1cdd0f5cda38ff735b8195e13fa
Eugene Rogov <eugeniirogov@MacBook-Pro-Eugenii.local>
2025-07-09 20:54:27 +0300

    Remove unnecessary comments from KoinInit.kt
    Add LoginViewModel to ViewModelModule.kt

--- composeApp/src/commonMain/kotlin/com/eugenerogov/planmind/KoinInit.kt
+++ composeApp/src/commonMain/kotlin/com/eugenerogov/planmind/KoinInit.kt
@@ -7,10 +7,6 @@ import com.eugenerogov.planmind.di.viewModelModule
 import org.koin.mp.KoinPlatformTools
 import org.koin.core.context.startKoin
 
-/**
- * Call [initKoin] once on each platform before using any dependency from Koin.
- * For iOS, call it from Swift (for example, inside `@main` App init or `SceneDelegate`).
- */
 fun initKoin() {
     // Avoid starting Koin twice (will throw an exception)
     if (KoinPlatformTools.defaultContext().getOrNull() != null) return
