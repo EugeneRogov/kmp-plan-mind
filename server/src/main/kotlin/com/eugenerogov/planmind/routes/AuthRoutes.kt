d2fef02036bfae63a12e0485af2efd3194c38b0d
Eugene Rogov <eugeniirogov@MacBook-Pro-Eugenii.local>
2025-07-10 19:57:46 +0300

    Implemented AUTH_REGISTER endpoint

--- server/src/main/kotlin/com/eugenerogov/planmind/routes/AuthRoutes.kt
+++ server/src/main/kotlin/com/eugenerogov/planmind/routes/AuthRoutes.kt
@@ -1,17 +1,20 @@
 package com.eugenerogov.planmind.routes
 
 import com.eugenerogov.planmind.JwtService
+import com.eugenerogov.planmind.data.remote.core.Endpoint.AUTH_REGISTER
 import com.eugenerogov.planmind.data.remote.core.Endpoint.AUTH_SIGN_IN
 import com.eugenerogov.planmind.data.remote.core.Endpoint.DEBUG_EMAIL
 import com.eugenerogov.planmind.data.remote.core.Endpoint.DEBUG_PASSWORD
 import com.eugenerogov.planmind.domain.entities.auth.LoginRequest
 import com.eugenerogov.planmind.domain.entities.auth.LoginResponse
-import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
-import io.ktor.server.request.*
+import com.eugenerogov.planmind.domain.entities.auth.RegisterRequest
+import io.ktor.server.request.receive
+import io.ktor.server.response.respond
+import io.ktor.server.routing.Route
+import io.ktor.server.routing.post
 
 fun Route.authRoutes() {
+
     post(AUTH_SIGN_IN) {
         val login = call.receive<LoginRequest>()
         if (login.email == DEBUG_EMAIL && login.password == DEBUG_PASSWORD) {
@@ -19,6 +22,16 @@ fun Route.authRoutes() {
             call.respond(LoginResponse(token = token))
         } else {
             call.respond(mapOf("error" to "Invalid credentials"))
+        }
+    }
+
+    post(AUTH_REGISTER) {
+        val register = call.receive<RegisterRequest>()
+        if (register.email == DEBUG_EMAIL) {
+            call.respond(mapOf("error" to "User already exists"))
+        } else {
+            val token = JwtService().generateToken(register.email)
+            call.respond(LoginResponse(token = token))
         }
     }
 }
